f66fec59428b4499e4ab8750a2b5ec22d7af1ade
ssahib <shivankaul.1993@gmail.com>
2018-03-18 11:00:06 +0000

    Detect RTC

--- rtc-api.js
+++ rtc-api.js
@@ -1,30 +1,57 @@
-
 function getLocalIPs(callback) {
 
     var ips = [];
 
     var pc = new RTCPeerConnection();
 
-    // Candidate found!
-    pc.onicecandidate = function(e) {
-        if (!e.candidate) { // Candidate gathering completed.
-            pc.close();
-            callback(ips);
+    return new Promise((resolve, reject) => {
+
+        // Candidate found!
+        pc.onicecandidate = function(e) {
+            if (!e.candidate) { // Candidate gathering completed.
+                pc.close();
+                resolve(ips);
+                return;
+            }
+            var ip = e.candidate.ip;
+            // var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+            if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+                ips.push(ip);
+        };
+
+        // Enable candidate gathering
+        pc.createDataChannel('');
+
+        pc.createOffer()
+            .then(function(offer) {
+                return pc.setLocalDescription(offer);
+            })
+            .catch(reason => reject(reason));
+    });
+}
+
+
+/* https://github.com/muaz-khan/DetectRTC/blob/master/DetectRTC.js#L839:53 */
+function detectRTC() {
+    var isWebRTCSupported = false;
+    ['RTCPeerConnection',
+        'webkitRTCPeerConnection',
+        'mozRTCPeerConnection',
+        'RTCIceGatherer'
+    ].forEach(function(item) {
+        if (isWebRTCSupported) {
             return;
         }
-        var ip = e.candidate.ip;
-        // var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
-            ips.push(ip);
-    };
 
-    // Enable candidate gathering
-    pc.createDataChannel('');
-
-    pc.createOffer().then(function(offer) {
-      return pc.setLocalDescription(offer);
-    })
-    .catch(function(reason) {
-      console.log(reason);
+        if (item in window) {
+            isWebRTCSupported = true;
+        }
+    });
+    return new Promise((resolve, reject) => {
+        if (isWebRTCSupported) {
+            resolve(isWebRTCSupported);
+        } else {
+            reject(isWebRTCSupported);
+        }
     });
 }
