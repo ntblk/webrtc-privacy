b237cb0c6ec608279f7a45d7df94af27c28acc6c
ssahib <shivankaul.1993@gmail.com>
2018-03-18 08:54:58 +0000

    Print out audio perm

--- rtc-api.js
+++ rtc-api.js
@@ -1,29 +1,25 @@
 
 function getLocalIPs(callback) {
+
     var ips = [];
 
-    var RTCPeerConnection = window.RTCPeerConnection ||
-        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+    var pc = new RTCPeerConnection();
 
-    var pc = new RTCPeerConnection({
-        // Don't specify any stun/turn servers, otherwise you will
-        // also find your public IP addresses.
-        iceServers: []
-    });
-    // Add a media line, this is needed to activate candidate gathering.
-    pc.createDataChannel('');
-
-    // onicecandidate is triggered whenever a candidate has been found.
+    // Candidate found!
     pc.onicecandidate = function(e) {
         if (!e.candidate) { // Candidate gathering completed.
             pc.close();
             callback(ips);
             return;
         }
-        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        var ip = e.candidate.ip;
+        // var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
         if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
             ips.push(ip);
     };
+
+    // Enable candidate gathering
+    pc.createDataChannel('');
 
     pc.createOffer().then(function(offer) {
       return pc.setLocalDescription(offer);
